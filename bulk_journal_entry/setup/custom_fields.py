8cf8b745197d390401d13ae15fb19461bdc149a5
DevCiel <developer@devciel.com>
2026-05-07 12:33:26 +0530

    feat: update custom fields setup

--- bulk_journal_entry/setup/custom_fields.py
+++ bulk_journal_entry/setup/custom_fields.py
@@ -1,11 +1,16 @@
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
+from .utils import has_doctype
+
 
 def setup_custom_fields():
 	create_custom_fields(get_custom_fields(), update=True)
 
 
 def get_custom_fields():
+	if not has_doctype("Patient"):
+		return {}
+
 	return {
 		"Journal Entry": [
 			{
