579dcf20a112360e96e86bf7406750ac5ea90a4d
DevCiel <developer@devciel.com>
2026-05-07 12:33:30 +0530

    refactor: improve setup utilities

--- bulk_journal_entry/setup/utils.py
+++ bulk_journal_entry/setup/utils.py
@@ -18,3 +18,7 @@ def ensure_module_def(module_name: str, app_name: str):
 				"app_name": app_name,
 			}
 		).insert(ignore_permissions=True)
+
+
+def has_doctype(doctype_name: str) -> bool:
+	return bool(frappe.db.exists("DocType", doctype_name))
